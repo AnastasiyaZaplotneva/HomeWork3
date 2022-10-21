8f50365bfd41b0449a3ebe90ec93c283e6aeb642
Anastasiya Z <nastya_tim2201@mail.ru>
2022-10-21 14:25:32 +0300

    решили третью задачу

---
+++ TaskThree/Program.cs
@@ -0,0 +1,12 @@
+﻿// SНапишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int count = 1;
+while(count <= number)
+{
+    Console.Write($"{Math.Pow(count, 3)}, ");
+    count++;
+}
